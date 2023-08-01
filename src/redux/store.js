1585cb899895e093c9089aac597c0470234b971b
akbrsazish <sazishaliakbar@gmail.com>
2023-08-01 21:34:45 +0330

    import bookreducer form bookSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { booksReducer } from './books/booksSlice'
 const store = configureStore({
   reducer: {
+    books: booksReducer,
   },
 });
 
