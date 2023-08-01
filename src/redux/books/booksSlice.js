bd39ab111599814cd857339d6f0dab0f7cb03954
akbrsazish <sazishaliakbar@gmail.com>
2023-08-01 21:23:04 +0330

    configure redux store

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const books = [
+    {
+      id: 1,
+      title: 'Java',
+      author: 'Ali',
+    },
+    {
+      id: 2,
+      title: 'JavaScript',
+      author: 'Jhon',
+    },
+    {
+      id: 3,
+      title: 'PHP',
+      author: 'foo',
+    },
+  ];
